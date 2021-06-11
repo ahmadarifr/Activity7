2cb55e5efc6e1f654a942b7d11ec83555d541c62
ahmadarifr <ahmad.arif.2016@ft.umy.ac.id>
2021-06-11 16:57:20 +0700

    Initial commit

---
+++ app/src/main/java/com/example/newsqlite/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.newsqlite;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.newsqlite.adapter.TemanAdapter;
+import com.example.newsqlite.database.DBController;
+import com.example.newsqlite.database.Teman;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private TemanAdapter adapter;
+    private ArrayList<Teman> temanArrayList;
+    DBController controller = new DBController(this);
+    String id,nm,tlp;
+    private FloatingActionButton fab;
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private static String url_select "http://10.0.2.2/umyTI/bacateman.php";
+    public static final String TAG_ID = "id";
+    public static final String TAG_NAMA = "nama";
+    public static final String TAG_TELPON = "telpon";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        fab = findViewById(R.id.floatingBtn);
+        //BacaData();
+        adapter = new TemanAdapter(temanArrayList);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(MainActivity.this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, TemanBaru.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+    public void BacaData(){
+        ArrayList<HashMap<String, String>>daftarTeman = controller.getAllTeman();
+        temanArrayList = new ArrayList<>();
+
+        for(int i=0;i<daftarTeman.size();i++){
+            Teman teman = new Teman();
+            teman.setId(daftarTeman.get(i).get("id").toString());
+            teman.setNama(daftarTeman.get(i).get("nama").toString());
+            teman.setTelpon(daftarTeman.get(i).get("telpon").toString());
+            temanArrayList.add(teman);
+        }
+    }
+}
